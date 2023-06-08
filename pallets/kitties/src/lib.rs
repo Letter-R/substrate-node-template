eae895843ed552a2919ae5f42832de5f3df6af37
letter <letter>
2023-06-08 20:38:40 +0800

    v0->v1

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -5,24 +5,29 @@
 /// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
 
+mod migrations;
+/*
 #[cfg(test)]
 mod mock;
 #[cfg(test)]
 mod tests;
-
+*/
 #[frame_support::pallet]
 pub mod pallet {
 
 	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+	use frame_system::pallet_prelude::{BlockNumberFor, *};
 
-	use frame_support::traits::{Currency, ExistenceRequirement, Randomness};
+	use crate::migrations;
+	use frame_support::{
+		traits::{Currency, ExistenceRequirement, Randomness},
+		PalletId,
+	};
 	use sp_io::hashing::blake2_128;
 	use sp_runtime::traits::AccountIdConversion;
 
-	use frame_support::PalletId;
-
 	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	pub struct Pallet<T>(_);
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -42,7 +47,13 @@ pub mod pallet {
 	#[derive(TypeInfo)] // SCALE's type info
 	#[derive(MaxEncodedLen)] //encode need know the length
 	#[derive(Clone, Copy, PartialEq, Eq, Default)]
-	pub struct Kitty(pub [u8; 16]);
+	//pub struct Kitty(pub [u8; 16]);
+	pub struct Kitty {
+		pub dna: [u8; 16],
+		pub name: [u8; 4],
+	}
+
+	pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 
 	//
 	pub type KittyId = u32;
@@ -94,15 +105,24 @@ pub mod pallet {
 		LowBalance,
 	}
 
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_runtime_upgrade() -> Weight {
+			migrations::v1::migrate::<T>()
+		}
+	}
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		/// create a kitty
 		#[pallet::call_index(0)]
 		#[pallet::weight(0)]
-		pub fn create(origin: OriginFor<T>) -> DispatchResult {
+		pub fn create(origin: OriginFor<T>, name: [u8; 4]) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			let kitty_id = Self::get_next_id()?;
-			let kitty = Kitty(Self::random_value(&who));
+
+			let dna = Self::random_value(&who);
+			let kitty = Kitty { dna, name };
 
 			Kitties::<T>::insert(kitty_id, kitty);
 			KittyOwner::<T>::insert(kitty_id, &who);
@@ -128,6 +148,7 @@ pub mod pallet {
 			origin: OriginFor<T>,
 			kitty_id_1: KittyId,
 			kitty_id_2: KittyId,
+			name: [u8; 4],
 		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
@@ -141,10 +162,10 @@ pub mod pallet {
 
 			let selector = Self::random_value(&who);
 			let mut data = [0u8; 16];
-			for i in 0..kitty_1.0.len() {
-				data[i] = (kitty_1.0[i] & selector[i]) | (kitty_2.0[i] & selector[i]);
+			for i in 0..kitty_1.dna.len() {
+				data[i] = (kitty_1.dna[i] & selector[i]) | (kitty_2.dna[i] & selector[i]);
 			}
-			let kitty = Kitty(data);
+			let kitty = Kitty { dna: data, name };
 
 			let price = T::KittyPrice::get();
 			let balance = T::Currency::free_balance(&who);
