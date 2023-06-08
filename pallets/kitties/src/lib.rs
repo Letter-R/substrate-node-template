dfd7e40844efa1ea6c1270e8df4e25a5bab95a1d
letter <letter>
2023-06-08 21:16:27 +0800

    v1->v2

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -50,10 +50,10 @@ pub mod pallet {
 	//pub struct Kitty(pub [u8; 16]);
 	pub struct Kitty {
 		pub dna: [u8; 16],
-		pub name: [u8; 4],
+		pub name: [u8; 8],
 	}
 
-	pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+	pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
 
 	//
 	pub type KittyId = u32;
@@ -108,7 +108,7 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn on_runtime_upgrade() -> Weight {
-			migrations::v1::migrate::<T>()
+			migrations::v2::migrate::<T>()
 		}
 	}
 
@@ -117,7 +117,7 @@ pub mod pallet {
 		/// create a kitty
 		#[pallet::call_index(0)]
 		#[pallet::weight(0)]
-		pub fn create(origin: OriginFor<T>, name: [u8; 4]) -> DispatchResult {
+		pub fn create(origin: OriginFor<T>, name: [u8; 8]) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			let kitty_id = Self::get_next_id()?;
 
@@ -148,7 +148,7 @@ pub mod pallet {
 			origin: OriginFor<T>,
 			kitty_id_1: KittyId,
 			kitty_id_2: KittyId,
-			name: [u8; 4],
+			name: [u8; 8],
 		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
--- pallets/kitties/src/migrations/v2.rs
+++ pallets/kitties/src/migrations/v2.rs
@@ -5,13 +5,16 @@ use crate::*;
 #[derive(
 	Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen, Clone, Copy, PartialEq, Eq, Default,
 )]
-pub struct OldKitty(pub [u8; 16]);
+pub struct OldKitty {
+	pub dna: [u8; 16],
+	pub name: [u8; 4],
+}
 
 pub fn migrate<T: Config>() -> Weight {
 	let on_chain_version = Pallet::<T>::on_chain_storage_version();
 	let current_version = Pallet::<T>::current_storage_version();
 
-	assert!((on_chain_version == 0) && (current_version == 1));
+	assert!((on_chain_version == 1) && (current_version == 2));
 
 	let module = Kitties::<T>::module_prefix();
 	let item = Kitties::<T>::storage_prefix();
@@ -19,7 +22,7 @@ pub fn migrate<T: Config>() -> Weight {
 	for (index, kitty) in
 		storage_key_iter::<KittyId, OldKitty, Blake2_128Concat>(module, item).drain()
 	{
-		let new_kitty = Kitty { dna: kitty.0, name: *b"abcd" };
+		let new_kitty = Kitty { dna: kitty.dna, name: *b"abcdefgh" };
 
 		Kitties::<T>::insert(index, &new_kitty);
 	}
