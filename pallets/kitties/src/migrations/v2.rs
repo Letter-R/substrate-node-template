363a4493c44fb70e137d7b6d06ce67c09289a13a
letter <letter>
2023-06-08 21:43:39 +0800

    v0->v2

--- pallets/kitties/src/migrations/v2.rs
+++ pallets/kitties/src/migrations/v2.rs
@@ -5,6 +5,31 @@ use crate::*;
 #[derive(
 	Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen, Clone, Copy, PartialEq, Eq, Default,
 )]
+pub struct OldKitty(pub [u8; 16]);
+
+pub fn migrate<T: Config>() -> Weight {
+	let on_chain_version = Pallet::<T>::on_chain_storage_version();
+	let current_version = Pallet::<T>::current_storage_version();
+
+	assert!((on_chain_version == 0) && (current_version == 2));
+
+	let module = Kitties::<T>::module_prefix();
+	let item = Kitties::<T>::storage_prefix();
+
+	for (index, kitty) in
+		storage_key_iter::<KittyId, OldKitty, Blake2_128Concat>(module, item).drain()
+	{
+		let new_kitty = Kitty { dna: kitty.0, name: *b"abcdefgh" };
+
+		Kitties::<T>::insert(index, &new_kitty);
+	}
+	Weight::zero()
+}
+
+/*
+#[derive(
+	Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen, Clone, Copy, PartialEq, Eq, Default,
+)]
 pub struct OldKitty {
 	pub dna: [u8; 16],
 	pub name: [u8; 4],
@@ -28,3 +53,5 @@ pub fn migrate<T: Config>() -> Weight {
 	}
 	Weight::zero()
 }
+
+ */
